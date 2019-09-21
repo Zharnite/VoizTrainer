27a3d3c3a0417e1f17c1b2647a7db32cb42bf35d
Zhen Yi Pan <zharnite@gmail.com>
2019-09-21 05:40:23 -0400

    typo

--- js/main.js
+++ js/main.js
@@ -82,7 +82,7 @@ function drawLoop( time ) {
 
     else {
     	canvasContext.fillStyle = "red";
-    	setTimeout(fuction(){
+    	setTimeout(function(){
     		navigator.vibrate(1000);
     	}, 7000);
 
